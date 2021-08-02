18fbff36519ba160d4f22281c81ca3e61b28d53b
molniya0207 <molniya0207@gmail.com>
2021-08-02 22:09:06 +0600

    1

---
+++ PowerupParty/GiveGUI.cs
@@ -0,0 +1,106 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Terrain.Packets;
+
+namespace PowerupParty
+{
+    [HarmonyPatch]
+    class GiveGUI
+    {
+        static GameObject playerSelectUI;
+        static int currPUI;
+
+        [HarmonyPatch(typeof(PowerupUI), "AddPowerup")]
+        [HarmonyPostfix]
+        static void AddPowerupToUI(ref int powerupId)
+        {
+            /**/
+            foreach (KeyValuePair<int, GameObject> i in PowerupUI.Instance.powerups)
+            {
+                Button btnComponent;
+                if (!i.Value.TryGetComponent(out btnComponent))
+                {
+                    i.Value.AddComponent<Button>();
+                    i.Value.TryGetComponent(out btnComponent);
+                }
+                btnComponent.onClick.RemoveAllListeners();
+                btnComponent.onClick.AddListener(delegate ()
+                {
+                    currPUI = i.Key;
+                    playerSelectUI.SetActive(true);
+                });
+            }
+        }
+
+        [HarmonyPatch(typeof(GameManager), "StartGame")]
+        [HarmonyPostfix]
+        static void GameStartPatch()
+        {
+            playerSelectUI = new GameObject("playerSelectUI", new[] { typeof(RectTransform), typeof(CanvasRenderer), typeof(RawImage), typeof(VerticalLayoutGroup) });
+            playerSelectUI.GetComponent<RectTransform>().position = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+            playerSelectUI.GetComponent<RectTransform>().sizeDelta = new Vector2(500, 500);
+            playerSelectUI.layer = 10;
+            playerSelectUI.transform.parent = GameObject.Find("/UI (1)").transform;
+            playerSelectUI.GetComponent<RawImage>().color = new Color32(80, 80, 80, 180);
+            playerSelectUI.SetActive(false);
+            playerSelectUI.GetComponent<VerticalLayoutGroup>().padding = new RectOffset(15, 15, 15, 15);
+            playerSelectUI.GetComponent<VerticalLayoutGroup>().spacing = 2;
+            playerSelectUI.GetComponent<VerticalLayoutGroup>().childAlignment = TextAnchor.UpperCenter;
+            playerSelectUI.GetComponent<VerticalLayoutGroup>().childControlWidth = true;
+            for (int y = 0; y < NetworkController.Instance.nPlayers; y++)
+            {
+                //if (y == LocalClient.instance.myId) continue;
+                GameObject plybtn = new GameObject("plybtn", new[] { typeof(RectTransform), typeof(CanvasRenderer), typeof(RawImage), typeof(Button) });
+                plybtn.transform.parent = playerSelectUI.transform;
+                plybtn.GetComponent<RawImage>().color = new Color(0.4f, 0.4f, 0.4f, 1f);
+                GameObject plybtnText = new GameObject("plybtnText", new[] { typeof(TextMeshProUGUI) });
+                plybtnText.transform.parent = plybtn.transform;
+                plybtnText.GetComponent<TextMeshProUGUI>().text = NetworkController.Instance.playerNames[y] + " (" + y.ToString() + ")";
+                plybtnText.GetComponent<TextMeshProUGUI>().fontSize = 16;
+                plybtnText.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Bold;
+                plybtnText.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, 1);
+                plybtn.GetComponent<Button>().onClick.RemoveAllListeners();
+                plybtn.GetComponent<Button>().onClick.AddListener(delegate ()
+                {
+                    int z = y;
+                    if (PowerupInventory.Instance.powerups[currPUI] > 0)
+                    {
+                        PowerupInventory.Instance.powerups[currPUI]--;
+                        TextMeshProUGUI componentInChildren = PowerupUI.Instance.powerups[currPUI].GetComponentInChildren<TextMeshProUGUI>();
+                        int num = int.Parse(componentInChildren.text);
+                        num--;
+                        componentInChildren.text = num.ToString();
+                        // TODO: Send add powerup packet
+                        using (PowerupParty.packets.WriteToServer("SendPowerupToClient", out var writer))
+                        {
+                            Debug.Log(z);
+                            writer.Write(z);
+                            writer.Write(currPUI);
+                        }
+                    }
+                });
+            }
+            GameObject closebtn = new GameObject("closebtn", new[] { typeof(RectTransform), typeof(CanvasRenderer), typeof(RawImage), typeof(Button) });
+            closebtn.transform.parent = playerSelectUI.transform;
+            closebtn.GetComponent<RawImage>().color = new Color(0.4f, 0.4f, 0.4f, 1f);
+            GameObject closebtnText = new GameObject("closebtnText", new[] { typeof(TextMeshProUGUI) });
+            closebtnText.transform.parent = closebtn.transform;
+            closebtnText.GetComponent<TextMeshProUGUI>().text = "Close";
+            closebtnText.GetComponent<TextMeshProUGUI>().fontSize = 16;
+            closebtnText.GetComponent<TextMeshProUGUI>().fontStyle = FontStyles.Bold;
+            closebtnText.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, 1);
+            closebtn.GetComponent<Button>().onClick.RemoveAllListeners();
+            closebtn.GetComponent<Button>().onClick.AddListener(delegate ()
+            {
+                playerSelectUI.SetActive(false);
+            });
+        }
+    }
+}
---
+++ PowerupParty/PowerupParty.cs
@@ -0,0 +1,56 @@
+﻿using BepInEx;
+using HarmonyLib;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terrain.Packets;
+using UnityEngine;
+
+// test seed -1461761351
+
+namespace PowerupParty
+{
+    [BepInPlugin("st.powerupparty", "PowerupParty", "1.0.0")]
+    class PowerupParty : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("PowerupParty");
+        public static OffroadPackets packets = new OffroadPackets("st.powerupparty");
+
+        public void Awake()
+        {
+            Logger.LogInfo("PowerupParty loaded!");
+
+            harmony.PatchAll();
+            packets.HandleClient("GetPowerupFromServer", ClientGetPowerupFromServer);
+            packets.HandleServer("SendPowerupToClient", ServerSendPowerupToClient);
+        }
+
+        public static void ServerSendPowerupToClient(int fromClient, BinaryReader br)
+        {
+            Debug.Log("JJJJJJJJJJJJJJJJJ");
+            int idTo = br.ReadInt32();
+            Debug.Log(idTo);
+            int powerupId = br.ReadInt32();
+            Debug.Log(idTo);
+            using (packets.WriteToClient("GetPowerupFromServer", idTo, out var writer, P2PSend.Reliable))
+            {
+                Debug.Log(idTo);
+                writer.Write(fromClient);
+                writer.Write(powerupId);
+                Debug.Log(idTo);
+            }
+        }
+
+        public static void ClientGetPowerupFromServer(BinaryReader br)
+        {
+            int idFrom = br.ReadInt32();
+            int powerupId = br.ReadInt32();
+            ClientSend.SendChatMessage("<color=#00FF00>" + NetworkController.Instance.playerNames[idFrom] + " gived powerup " + ItemManager.Instance.allPowerups[powerupId].name + " to " + NetworkController.Instance.playerNames[LocalClient.instance.myId] + "!");
+            ChatBox.Instance.AppendMessage(-1, "<color=#00FF00>You received powerup " + ItemManager.Instance.allPowerups[powerupId].name + " from " + NetworkController.Instance.playerNames[idFrom] + "!", "");
+        }
+    }
+}
